7a53c0d4b791128a6858ef82cd809bcc0707a6c4
Anuj Shrestha <anujshrestha@lftechnology.com>
2020-08-07 17:32:19 +0545

    Get build files out for gh pages

---
+++ build/precache-manifest.e22cf4e7dbf122937d71461e25b310b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8061e098424d7375f6711549ed33337",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d87e5876498aa7b9230f",
+    "url": "./static/css/main.47e1cbe0.chunk.css"
+  },
+  {
+    "revision": "a3bfc775ac50ffe72b1d",
+    "url": "./static/js/2.f88ccbc0.chunk.js"
+  },
+  {
+    "revision": "d87e5876498aa7b9230f",
+    "url": "./static/js/main.2d79dc74.chunk.js"
+  },
+  {
+    "revision": "92b7010270b0ac324f72",
+    "url": "./static/js/runtime~main.dd97c86a.js"
+  }
+]);
